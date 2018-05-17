74daaca36fb586449a4df8866f36d1b2a5151918
iMassim0 <i.max@live.fr>
2018-05-17 14:43:29 +0200

    waouh ca fonctionne pas du tout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'home/lp1'
-  get 'home/lp2'
-  get 'home/lp3'
+  root 'home#index'
+  get '/1', to: "home#lp1", as: "land1"
+  get '/2', to: "home#lp2", as: "land2"
+  get '/3', to: "home#lp3", as: "land3"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
