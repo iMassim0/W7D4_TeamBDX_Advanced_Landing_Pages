6368f897681b6511308d09ce8a8725417e7bc856
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-05-17 11:28:23 +0200

    init service bot_twitter

---
+++ advanced-landing-pages/app/services/bot_twitter.rb
@@ -0,0 +1,13 @@
+class BotTwitter
+
+  def initialize
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_CONSUMER']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = ENV['TWITTER_TOKEN']
+      config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
+  end
+
+  
+
+end
