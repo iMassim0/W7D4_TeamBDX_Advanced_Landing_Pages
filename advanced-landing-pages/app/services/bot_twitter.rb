b57a36e3c90199b52c8ad275b0ecee94fd41ef22
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-05-17 12:25:49 +0200

    add model Tweet + service bot_twitter

--- advanced-landing-pages/app/services/bot_twitter.rb
+++ advanced-landing-pages/app/services/bot_twitter.rb
@@ -8,12 +8,25 @@ class BotTwitter
       config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
   end
 
-
-  def 
+  def search_tweets
+    client.search("#formation #web -rt", lang: "fr").take(10).each do |tweet|
+      Tweet.create(tweet_id: tweet.id, creator: tweet.user.screen_name, date: tweet.created_at)
+    end
+    p "Il y a #{Tweet.find_by(:reply? 0).size} tweets à traiter en base de données."
   end
 
+  def send_tweets
+    Tweet.find_by(:reply? 0).each do |tweet|
+      client.update("@#{tweet.creator} Découvrez The Hacking Project, une formation développeur·euse web gratuite. Rendez-vous ici ! https://bdx-landingpages.herokuapp.com")
+      tweet.reply? = true
+      p "Tweet envoyé à #{tweet.creator}."
+      sleep(60)
+    end
+  end
 
   def perform
+    search_tweets
+    send_tweets
   end
 
 end
---
+++ advanced-landing-pages/db/migrate/20180517101513_create_tweets.rb
@@ -0,0 +1,13 @@
+class CreateTweets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tweets do |t|
+      t.integer :tweet_id, null: false
+      t.string :creator, null: false
+      t.datetime :date
+      t.boolean :reply?, default: 0
+
+      t.timestamps
+    end
+      add_index :tweets, :tweet_id, unique: true
+  end
+end
